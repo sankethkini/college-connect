bfc4011d3883c04c906d46203ebee9d915e9a8e8
Sanketh Kini <kinisanketh@gmail.com>
2021-03-03 12:17:12 +0530

    Update settings.py

--- college/college/settings.py
+++ college/college/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'college',
         'USER':'root',
-        'PASSWORD':'sanketh123',
+        'PASSWORD':'#########',
         'HOST':'localhost',
         'PORT':'3306'
     }
